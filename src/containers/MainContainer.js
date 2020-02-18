d3a0901a6e9cd18a0e2e763109db3f6e5e3915e1
Lilaro <larochelle.lisa@gmail.com>
2020-02-18 15:14:34 -0500

    spacing

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -7,90 +7,90 @@ import ballotBox from '../BallotIcon.svg'
 export class MainContainer extends Component {
 
     state = {
-        searchTerm: ""
+      searchTerm: ""
     }
 
     //route to chat page when user clicks 'chat' button on selected site's popup
     handleSiteClick = (e, site) => {
-        e.preventDefault()
-        this.props.setSelectedSite(site)
-        this.props.history.push(`/chat/${site.id}`)          
+      e.preventDefault()
+      this.props.setSelectedSite(site)
+      this.props.history.push(`/chat/${site.id}`)          
     }
     
     //Filter through all poll sites by entrance address by comparing to searchTerm
     searchResults = (searchWord) => {
-        if (this.props.sites) {
-            return this.props.sites.filter(site => {
-                if (site.voter_entrance && this.searchTerm !== ""){
-                    return site.voter_entrance.includes(searchWord)}
-            })
-        } 
+      if (this.props.sites) {
+        return this.props.sites.filter(site => {
+          if (site.voter_entrance && this.searchTerm !== ""){
+          return site.voter_entrance.includes(searchWord)}
+        })
+      } 
     }
     
     //Populate searchTerm in state as user types
     //and pass to searchResults function
     handleChange = (e) => {   
-        this.setState({
-            ...this.state,
-            searchTerm: e.target.value,
-        }, () => this.searchResults(this.state.searchTerm))
+      this.setState({
+      ...this.state,
+      searchTerm: e.target.value,
+      }, () => this.searchResults(this.state.searchTerm))
     }
 
     logoutClicked = (e) => {
-        e.preventDefault()      
-        this.props.handleLogout()
-        this.props.history.push('/login')
+      e.preventDefault()      
+      this.props.handleLogout()
+      this.props.history.push('/login')
     }
     
     profileClicked = (e) => {
-        e.preventDefault()
-        this.props.history.push('/profile')
+      e.preventDefault()
+      this.props.history.push('/profile')
     }
   
     render() {
 
     return (
-        <>
-            <Segment clearing>
-                <Header as='h3' floated='left'>
-                    <img src={ballotBox} alt="Ballot Box Icon"  style={{height: '40px', width: '40px'}}/>
-                    Poll Check
-                </Header>
-                <Header floated='right'>
-                    <Breadcrumb floated='right'>
-                        <Breadcrumb.Section link onClick={this.profileClicked}><h4>my account</h4></Breadcrumb.Section>
-                        <Breadcrumb.Divider>/</Breadcrumb.Divider>
-                        <Breadcrumb.Section link onClick={this.logoutClicked}><h4>logout</h4></Breadcrumb.Section>
-                    </Breadcrumb>    
-                </Header>
-            </Segment>
+      <>
+         <Segment clearing>
+           <Header as='h3' floated='left'>
+             <img src={ballotBox} alt="Ballot Box Icon"  style={{height: '40px', width: '40px'}}/>
+                  Poll Check
+           </Header>
+           <Header floated='right'>
+             <Breadcrumb floated='right'>
+             <Breadcrumb.Section link onClick={this.profileClicked}><h4>my account</h4></Breadcrumb.Section>
+             <Breadcrumb.Divider>/</Breadcrumb.Divider>
+             <Breadcrumb.Section link onClick={this.logoutClicked}><h4>logout</h4></Breadcrumb.Section>
+             </Breadcrumb>    
+           </Header>
+         </Segment>
                 
-            <Map
-                sites={this.props.sites} 
-                name={this.props.name}
-                token={this.props.token}
-                channel={this.props.channel}
-                userId={this.props.userId}
-                messages={this.props.messages}
-                displayedSite={this.props.site}
-                siteClicked={this.props.siteClicked}
-                selectedSite={this.props.selectedSite}
-                setSelectedSite={this.props.setSelectedSite}
-                handleSiteClick={this.handleSiteClick}
-                currentUser={this.props.currentUser}
-                submitMessage={this.props.submitMessage}
-                messageChange={this.props.messageChange}
-                searchTerm={this.state.searchTerm}
-                handleChange={this.handleChange}
-                searchResults={this.searchResults}
-                count={this.state.count}
-                color={this.state.color}
-                alertClick={this.alertClick}
-            >
-            </Map>           
-       </>
-        )
-    }
+          <Map
+              sites={this.props.sites} 
+              name={this.props.name}
+              token={this.props.token}
+              channel={this.props.channel}
+              userId={this.props.userId}
+              messages={this.props.messages}
+              displayedSite={this.props.site}
+              siteClicked={this.props.siteClicked}
+              selectedSite={this.props.selectedSite}
+              setSelectedSite={this.props.setSelectedSite}
+              handleSiteClick={this.handleSiteClick}
+              currentUser={this.props.currentUser}
+              submitMessage={this.props.submitMessage}
+              messageChange={this.props.messageChange}
+              searchTerm={this.state.searchTerm}
+              handleChange={this.handleChange}
+              searchResults={this.searchResults}
+              count={this.state.count}
+              color={this.state.color}
+              alertClick={this.alertClick}
+          >
+          </Map>           
+      </>
+    )
+  }
 }
 
 export default withRouter(MainContainer)
