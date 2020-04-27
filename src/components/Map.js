c333afdf510790fa033622371e759b247ef3ec83
Lilaro <larochelle.lisa@gmail.com>
2020-04-26 22:38:08 -0400

    cleanup/spacing

--- src/components/Map.js
+++ src/components/Map.js
@@ -16,56 +16,54 @@ export default function Map(props) {
 
   const [clickededSite, setSite] = useState(null);
   
-  return (
-    
+  return (  
     <> 
-       <ReactMapGL {...viewport}
-          mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-          mapStyle="mapbox://styles/lisalarochelle/ck36bxl2r08n81cpi5b8l1u7v"
-          onViewportChange={(viewport) => {
-            setViewport(viewport)
-          }
-        } 
-        height='700px' paddingLeft='10px'> 
-        <Form.Input size='large' placeholder="Site Address" onChange={props.handleChange}
-               icon={{ name: 'search'}} style={{margin: '40px'}}/>
-        {props.searchResults(props.searchTerm).map((site) => (
-         <Marker key={site.id} latitude={parseFloat(site.latitude)} longitude={parseFloat(site.longitude)}>
+      <ReactMapGL {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        mapStyle="mapbox://styles/lisalarochelle/ck36bxl2r08n81cpi5b8l1u7v"
+        onViewportChange={(viewport) => {
+          setViewport(viewport)
+        }
+      } 
+      height='700px' paddingLeft='10px'> 
+      <Form.Input size='large' placeholder="Site Address" onChange={props.handleChange}
+       icon={{ name: 'search'}} style={{margin: '40px'}}/>
+      {props.searchResults(props.searchTerm).map((site) => (
+        <Marker key={site.id} latitude={parseFloat(site.latitude)} longitude={parseFloat(site.longitude)}>
           <div>
               <img src={ballotBox} alt="Ballot Box Icon" className="marker-button"
               onClick={(e) => setSite(site)}/>            
           </div>
-
-         </Marker>
-       ))}
-       
-       {clickededSite ? (
-          <Popup
-            key={clickededSite.id}
-            site={clickededSite} 
-            channel={props.channel}
-            messages={props.messages}
-            selectedSite={props.selectedSite}
-            siteClicked={props.siteClicked}
-            handleSiteClick={props.handleSiteClick}
-            latitude={parseFloat(clickededSite.latitude)}
-            longitude={parseFloat(clickededSite.longitude)}
-            onClose={() => {
-                setSite(null);
-              }}
-              >
-              <div>
-                <h4>{clickededSite.site_name}</h4>
-                <p>District {clickededSite.council_district}</p>
-                <p>{clickededSite.street_number + ' ' + clickededSite.street_name + ', ' + clickededSite.borough}</p>
-                <p><Icon name='wheelchair' color='teal'/> {clickededSite.handicap_entrance}</p>
-                <Button onClick={(e) => props.handleSiteClick(e, clickededSite)}>
-                      <Icon name='comments outline' />Chat</Button>
-              </div>
-            
-          </Popup>       
-          ) : null}
-       </ReactMapGL>
-      </>
-    )
+        </Marker>
+      ))}
+      
+      {clickededSite ? (
+        <Popup
+          key={clickededSite.id}
+          site={clickededSite} 
+          channel={props.channel}
+          messages={props.messages}
+          selectedSite={props.selectedSite}
+          siteClicked={props.siteClicked}
+          handleSiteClick={props.handleSiteClick}
+          latitude={parseFloat(clickededSite.latitude)}
+          longitude={parseFloat(clickededSite.longitude)}
+          onClose={() => {
+              setSite(null);
+            }}>
+            <div>
+              <h4>{clickededSite.site_name}</h4>
+              <p>District {clickededSite.council_district}</p>
+              <p>{clickededSite.street_number + ' ' + clickededSite.street_name + ', ' + clickededSite.borough}</p>
+              <p><Icon name='wheelchair' color='teal'/> {clickededSite.handicap_entrance}</p>
+              <Button onClick={(e) => props.handleSiteClick(e, clickededSite)}>
+                    <Icon name='comments outline' />Chat</Button>
+            </div>  
+        </Popup>
+      )
+        :
+        null}
+      </ReactMapGL>
+    </>
+  )
   }
